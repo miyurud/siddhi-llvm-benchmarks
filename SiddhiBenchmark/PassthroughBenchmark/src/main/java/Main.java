40f9699f4003bf197db2378081fde82bbbdbb565
BraveenSri <braveensri.15@cse.mrt.ac.lk>
2019-08-09 15:46:31 +0530

    Update Main.java

--- SiddhiBenchmark/PassthroughBenchmark/src/main/java/Main.java
+++ SiddhiBenchmark/PassthroughBenchmark/src/main/java/Main.java
@@ -1,3 +1,16 @@
+/**
+ Copyright 2019 Siddhi-LLVM Team
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -13,9 +26,9 @@ import org.wso2.siddhi.core.event.Event;
 import org.wso2.siddhi.core.stream.input.InputHandler;
 import org.wso2.siddhi.core.stream.output.StreamCallback;
 
-public class Main {
+public class Benchmark {
 
-    private static final Logger log = Logger.getLogger(Main.class);
+    private static final Logger log = Logger.getLogger(Benchmark.class);
     private static String logDir = "./PassthroughBenchmark/results";
     private static final int RECORD_WINDOW = 1000000; //This is the number of events to record.
     private static long eventCountTotal = 0;
